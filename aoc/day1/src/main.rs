e890e7ed7b12c50228485204d033ac4db81db35f
Srihari Radhakrishnan <sr@sriharis-mbp-2.lan>
2020-11-09 19:40:21 -0800

    finished part 2

--- aoc/day1/src/main.rs
+++ aoc/day1/src/main.rs
@@ -13,19 +13,27 @@ fn main() {
     let input = argument_set.value_of("input").unwrap();
     let code: String = fs::read_to_string(input)
                           .expect("unable to open");
-    let cur_floor = get_current_floor(code); 
-    println!("Floor is {}", cur_floor); // this cannot be formatted with default formatter if the
+    let (cur_floor, first_negative_floor_index) = get_current_floor(code); 
+    println!("Floor is {}; first negative floor index is {}", 
+                cur_floor, 
+                first_negative_floor_index); // this cannot be formatted with default formatter if the
                                         // return type is not explicitly defined in the function 
 }
 
-fn get_current_floor(code : String) -> i32 {
-    let mut floor: i32 = 0; // floor has to be mut to increment or decrement
+fn get_current_floor(code : String) -> (i32, i32) {
+    let mut final_floor: i32 = 0; // floor has to be mut to increment or decrement
+    let mut current_floor_index: i32 = 0;
+    let mut first_negative_floor_index: i32 = 0;
     for c in code.chars() { // maybe there's a select case way to do this that I don't know about
+        current_floor_index = current_floor_index + 1;
         if c == '(' {
-            floor = floor + 1;
+            final_floor = final_floor + 1;
         } else if c == ')' {
-            floor = floor - 1;
+            final_floor = final_floor - 1;
+        }
+        if final_floor < 0 && first_negative_floor_index == 0 {
+            first_negative_floor_index = current_floor_index
         }
     }
-   return floor;
+   return (final_floor, first_negative_floor_index);
 }
